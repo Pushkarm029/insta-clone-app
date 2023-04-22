340b1088b30344a3303627b3bc31b8b92b9fcdd2
Pushkar Mishra <pushkarmishra029@gmail.com>
2023-04-22 11:50:42 +0530

    added posts in home

--- src/data/account.js
+++ src/data/account.js
@@ -12,47 +12,95 @@ export const accountList = [{
         caption: 'Hello Hello Kon rabb Deep',
         comment: 'Nice Project'
     }]
-},
-{/* add postswith caption */},
-
-{
-    name: 'Kshitij',
-    id: '2022A2PS1698',
-    url: '',
-    description: '| 18 | Fresher @ BITS Pilani',
-    followers: '100',
-    following: '10',
-    posts: '1K'
 },{
-    name: 'Pushkar Mishra',
-    id: 'pushkarm029',
-    url: '',
-    description: 'Full Stack Web Developer | 18 | Fresher @ BITS Pilani https://github.com/@pushkarm029',
-    followers: '452',
-    following: '77',
-    posts: '8'
-},{
-    name: 'Pushkar Mishra',
-    id: 'pushkarm029',
-    url: '',
-    description: 'Full Stack Web Developer | 18 | Fresher @ BITS Pilani https://github.com/@pushkarm029',
-    followers: '452',
-    following: '77',
-    posts: '8'
-},{
-    name: 'Pushkar Mishra',
-    id: 'pushkarm029',
-    url: '',
-    description: 'Full Stack Web Developer | 18 | Fresher @ BITS Pilani https://github.com/@pushkarm029',
-    followers: '452',
-    following: '77',
-    posts: '8'
-},{
-    name: 'Pushkar Mishra',
-    id: 'pushkarm029',
-    url: '',
-    description: 'Full Stack Web Developer | 18 | Fresher @ BITS Pilani https://github.com/@pushkarm029',
-    followers: '452',
-    following: '77',
-    posts: '8'
-}];
+    name: 'Jane Doe',
+    id: 'janedoe1',
+    url: 'https://www.example.com/janedoe1',
+    description: 'Student | Artist | Dreamer',
+    followers: '2.5K',
+    following: '500',
+    posts: [
+      {
+        number: '5',
+        imageurl: 'https://www.example.com/images/post5.jpg',
+        likes: '34',
+        caption: 'New painting!',
+        comment: ''
+      },
+      {
+        number: '4',
+        imageurl: 'https://www.example.com/images/post4.jpg',
+        likes: '56',
+        caption: 'Happy Birthday to me! 🎉🎂',
+        comment: 'Wish you all the best!'
+      },
+      {
+        number: '3',
+        imageurl: '',
+        likes: '20',
+        caption: 'Just a random thought...',
+        comment: 'Interesting!'
+      }
+    ]
+  },
+  {
+    name: 'John Smith',
+    id: 'johnsmith',
+    url: 'https://www.example.com/johnsmith',
+    description: 'Photographer | Traveler | Foodie',
+    followers: '10K',
+    following: '1K',
+    posts: [
+      {
+        number: '12',
+        imageurl: '',
+        likes: '50',
+        caption: 'Can\'t wait to travel again!',
+        comment: 'Me too!'
+      },
+      {
+        number: '11',
+        imageurl: 'https://www.example.com/images/post11.jpg',
+        likes: '63',
+        caption: 'Beautiful sunset today!',
+        comment: ''
+      },
+      {
+        number: '10',
+        imageurl: 'https://www.example.com/images/post10.jpg',
+        likes: '72',
+        caption: 'Yummy food!',
+        comment: 'Looks delicious!'
+      }
+    ]
+  },
+  {
+    name: 'Linda Anderson',
+    id: 'lindaanderson',
+    url: 'https://www.example.com/lindaanderson',
+    description: 'Teacher | Writer | Mother',
+    followers: '1.2K',
+    following: '200',
+    posts: [
+      {
+        number: '3',
+        imageurl: '',
+        likes: '15',
+        caption: 'Just finished my new book!',
+        comment: ''
+      },
+      {
+        number: '2',
+        imageurl: 'https://www.example.com/images/post2.jpg',
+        likes: '27',
+        caption: 'Love teaching!',
+        comment: 'You are an amazing teacher!'
+      },
+      {
+        number: '1',
+        imageurl: '',
+        likes: '10',
+        caption: 'Happy Mother\'s Day!',
+        comment: "G8"
+      }]
+  }]
--- src/home/body/App.js
+++ src/home/body/App.js
@@ -1,39 +1,66 @@
-import './App.css';
-import {FiMoreHorizontal} from 'react-icons/fi';
-import { accountList } from './../../data/account';
+import "./App.css";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { accountList } from "./../../data/account";
+import {AiOutlineHeart} from "react-icons/ai";
+import {FiMessageSquare} from "react-icons/fi";
+import {RiShareForwardLine} from "react-icons/ri";
+import {FiBookmark} from "react-icons/fi";
 
 export default function Body() {
-    return (
-      <div className='body'>
-        <div className='stories'>
-          
-        </div>
-        <div className='posts'>
-          <div className='post'>
-            <div className='postheader'>
-              <img src = {accountList[1].url} alt = {accountList[1].id} />
-              <p>{accountList[1].id}</p>                 {/* username */}
-              <p> 1 Day Ago</p>                 {/* 1 day ago */}
-              <FiMoreHorizontal/>
+  return (
+    <div className="body">
+      <div className="stories">
+        {accountList.slice(1, 8).map((account) => (
+          <div key={account.id}>
+            <img src={account.url} alt={account.name} />
+            <p>{account.id}</p>
+            <p>{account.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="posts">
+        {accountList.slice(1).map((account) => (
+          <div className="post" key={account.id}>
+            <div className="postheader">
+              <img src={account.url} alt={account.id} />
+              <p>{account.id}</p>
+              <p>1 Day Ago</p>
+              <FiMoreHorizontal />
             </div>
-            <div className='postimage'>  {/* add multiple images */}
-              <img src={accountList[0].posts[0].imageurl} alt='' />
-            </div>
-            <p>{accountList[0].posts[0].number}</p> {/*code working*/}
-            <div className='interactablepost'>
-              <div className='interactablepostleft'></div>
-              <div className='interactablepostright'></div>
-            </div>
-            <div className='postfooter'>
-              <p></p>                 {/* 1 like */}
-              <div className='postfootercaption'>
-                <p></p>                 {/* username */}
-                <p></p>                 {/* caption */} 
-              </div>
-              <p></p>                   {/* 1 comment */}
+            <div>
+              {account.posts.length > 0 ? (
+                account.posts.map((post) => (
+                  <div key={post.number}>
+                    <div className="postimage">
+                      <img src={post.imageurl} alt="" />
+                    </div>
+                    <div className="interactablepost">
+                      <div className="interactablepostleft">
+                        <AiOutlineHeart/>
+                        <FiMessageSquare/>
+                        <RiShareForwardLine/>
+                      </div>
+                      <div className="interactablepostright">
+                        <FiBookmark/>
+                      </div>
+                    </div>
+                    <div className="postfooter">
+                      <p>{post.likes} likes</p>
+                      <div className="postfootercaption">
+                        <p>{account.name}</p>
+                        <p>{post.caption}</p>
+                      </div>
+                      <p>1 comment</p>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>No post available</p>
+              )}
             </div>
           </div>
-        </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
