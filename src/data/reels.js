041b2b3211a7d197aa0d370ebc1583dcfc3e878c
Pushkar Mishra <pushkarmishra029@gmail.com>
2023-05-11 01:51:35 +0530

    reels done

---
+++ src/data/reels.js
@@ -0,0 +1,25 @@
+export const reelsList = [{
+    imageurl : 'https://images.unsplash.com/photo-1683444805823-3de8d2fd1c80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    id : 'crazyProject',
+    description : 'Maina',
+    url : 'https://youtu.be/ET8-iGMow2A',
+    audio : 'Maina',
+    likes : '1M',
+    comments : '1K'
+},{
+    imageurl : 'https://images.unsplash.com/photo-1683538967101-a1543aac2dc9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    id : 'Talha Anjum',
+    description : 'Downers At Dusk | Prod. by Umair (Official Audio)',
+    url : 'https://youtu.be/iWomYr2dlsM',
+    audio : 'Downers At Dusk',
+    likes : '10K',
+    comments : '989'
+},{
+    imageurl : 'https://images.unsplash.com/photo-1683641709248-6be047dfe319?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+    id : 'Emiway Bantai',
+    description : 'Kya Bolti Public (Official Music Video) | Emiway Bantai Prod by %#^$#$',
+    url : 'https://youtu.be/uMcU5qVz9YE?list=RDzN38VR5Fr8I',
+    audio : 'Kya Bolti Public',
+    likes : '-100M',
+    comments : '10M'
+}]
--- src/reels/App.js
+++ src/reels/App.js
@@ -1,9 +1,65 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import  {BsMusicNoteList} from 'react-icons/bs';
+import {AiOutlineHeart} from 'react-icons/ai';
+import {FiMessageCircle} from 'react-icons/fi';
+import {FaTelegramPlane} from 'react-icons/fa';
+import {FiMoreHorizontal} from 'react-icons/fi';
+import {BsBookmark} from 'react-icons/bs';
 import './App.css';
+import { reelsList } from '../data/reels';
 
-export default function Body() {
+export default function Reels() {
     return (
-      <>
-      
-      </>
+      <div className='reels'>
+        {reelsList.map((reels) => (
+          <div className='reel'>
+            <div className='reelsSectionOne'>
+              <ReactPlayer 
+                className='reelVideoPlayer'
+                width='369px'
+                height='525px'
+                url= {reels.url}
+              />
+              <div className='reelsSectionOneHeader'>
+                <div className='reelsSectionOProfile'>
+                  <img src={reels.imageurl}></img>
+                  <p>{reels.id} &#183;</p>
+                  <h2>Follow</h2>
+                </div>
+                <div className='reelsCaption'>
+                  <p>{reels.description}</p>
+                </div>
+                <div className='reelsAudioTitle'>
+                  <BsMusicNoteList size={12} color="white" style={{padding :'2px'}}/>
+                  <p>{reels.audio}</p>
+                </div>
+              </div>
+            </div>
+            <div className='reelsSectionTwo'>
+              <div className='reelsSectionTwoLike'>
+                <AiOutlineHeart size={27} color="white" style={{padding :'2px'}}/>
+                <p>{reels.likes}</p>
+              </div>
+              <div className='reelsSectionTwoComments'>
+                <FiMessageCircle size={27} color="white" style={{padding :'2px'}}/>
+                <p>{reels.comments}</p>
+              </div>
+              <div className='reelsSectionTwoForward'>
+                <FaTelegramPlane size={27} color="white" style={{padding :'2px'}}/>
+              </div>
+              <div className='reelsSectionTwoBookmark'>
+                <BsBookmark size={27} color="white" style={{padding :'2px'}}/>
+              </div>
+              <div className='reelsSectionTwoMore'>
+                <FiMoreHorizontal size={27} color="white" style={{padding :'2px'}}/>
+              </div>
+              <div className='reelsSectionTwoAudioOwner'>
+                <img src={reels.imageurl}></img>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     );
   }
