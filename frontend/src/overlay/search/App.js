c995baf380fd7ce9f37dd6a3b707c907c6515409
Pushkar Mishra <pushkarmishra029@gmail.com>
2023-10-01 11:52:35 +0530

    tests for search

--- frontend/src/overlay/search/App.js
+++ frontend/src/overlay/search/App.js
@@ -6,34 +6,32 @@ export default function SearchPage() {
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
-  const [userData, setUserData] = useState([]); // Initialize as an empty array
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
     fetch(`/api/search/users`, {
       headers: {
-        Accept: 'application/json',
+        Accept: "application/json",
       },
-      signal, // Pass the signal to the fetch request
+      signal,
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log('API Response Data:', data);
+      .then((response) => response.json())
+      .then((data) => {
         setUserData(data);
       })
-      .catch(error => {
-        console.error('Error fetching images links:', error);
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
         setUserData([]);
       });
 
     return () => {
-      abortController.abort(); // Cancel the fetch request when the component unmounts
+      abortController.abort();
     };
   }, []);
 
   useEffect(() => {
-    // Filter the data based on the search query
     const filteredData = userData.filter((item) =>
       item[0].toLowerCase().includes(searchQuery.toLowerCase())
     );
@@ -43,9 +41,10 @@ export default function SearchPage() {
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
+
   const handleNavigation = (targetEmail) => {
-    navigate(`/profile?prop=${targetEmail}`)
-  }
+    navigate(`/profile?prop=${targetEmail}`);
+  };
 
   return (
     <div className="searchOverlay">
@@ -57,27 +56,38 @@ export default function SearchPage() {
           value={searchQuery}
           onChange={handleSearchChange}
           placeholder="Type to search..."
+          data-testid="search-input"
         />
         {searchQuery.length > 0 ? (
           filteredResults.length > 0 ? (
             <div className="searchResults">
               {filteredResults.map((result, index) => (
-                <div onClick={() => handleNavigation(result[4])} className="eachSearchResult" key={index}>
-                  <img src={result[3]} />
+                <div
+                  onClick={() => handleNavigation(result[4])}
+                  className="eachSearchResult"
+                  key={index}
+                >
+                  <img src={result[3]} alt={`Result ${index}`} />
                   <div className="eachSearchResultLeft">
                     <div className="eachSearchResultTop">
-                      <p><strong>{result[0]}</strong></p>
+                      <p>
+                        <strong>{result[0]}</strong>
+                      </p>
                     </div>
                     <div className="eachSearchResultBottom">
-                      <p className="eachSearchedUserName">{result[2]}</p>
-                      <p className="eachSearchedFollowers">{result[1]} Followers</p>
+                      <p className="eachSearchedUserName" data-testid="search-results">{result[2]}</p>
+                      <p className="eachSearchedFollowers">
+                        {result[1]} Followers
+                      </p>
                     </div>
                   </div>
                 </div>
               ))}
             </div>
           ) : (
-            <p><strong>{searchQuery}</strong> is currently not available on this platform</p>
+            <p>
+              {searchQuery} is currently not available on this platform
+            </p>
           )
         ) : (
           <p>No search results yet.</p>
@@ -85,4 +95,4 @@ export default function SearchPage() {
       </div>
     </div>
   );
-};
+}
